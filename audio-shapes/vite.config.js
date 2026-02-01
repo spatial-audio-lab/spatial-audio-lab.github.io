1b65cfc96b5b8b4d556a7793a5f016b2f02a3823
SaneFungus <63272836+SaneFungus@users.noreply.github.com>
2026-02-01 15:33:05 +0100

    Update vite.config.js

--- audio-shapes/vite.config.js
+++ audio-shapes/vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react"
 // WAŻNE: zmień 'spatial-audio-lab' na nazwę Twojego repo jeśli jest inna
 export default defineConfig(({ command }) => ({
   plugins: [react()],
-  base: command === "build" ? "/spatial-audio-lab.github.io/audio-shapes/" : "/",
+  base: command === "build" ? "/audio-shapes/" : "/",
 }))
